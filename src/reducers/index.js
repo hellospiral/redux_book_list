cd8749b9a909f18bd8a1d3df9762095ad8ac7551
Matt Carlson <matthewthomascarlson@gmail.com>
2017-09-14 13:34:16 -0700

    add books reducer, connect book-list container

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  // Adds a key to the global application state called "books", where the key is "books" and the value is whatever gets returned from the BooksReducer
+  books: BooksReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: "The Well Grounded Rubyist" },
+    { title: "Functional Javascript" },
+    { title: "Browser Networking" },
+    { title: 'Rails Antipatterns' }
+  ]
+}
