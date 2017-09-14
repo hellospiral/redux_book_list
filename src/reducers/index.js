d8442aa96b4ed17151c29a149db140a6add57c45
Matt Carlson <matthewthomascarlson@gmail.com>
2017-09-14 14:56:09 -0700

    show the details for a particular book on click

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
 
 const rootReducer = combineReducers({
   // Adds a key to the global application state called "books", where the key is "books" and the value is whatever gets returned from the BooksReducer
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function () {
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: "The Well Grounded Rubyist" },
-    { title: "Functional Javascript" },
-    { title: "Browser Networking" },
-    { title: 'Rails Antipatterns' }
+    { title: 'Javascript: The Good Parts', pages: 235 },
+    { title: "The Well Grounded Rubyist", pages: 543 },
+    { title: "Functional Javascript", pages: 254 },
+    { title: "Browser Networking", pages: 312 },
+    { title: 'Rails Antipatterns', pages: 284 }
   ]
 }
